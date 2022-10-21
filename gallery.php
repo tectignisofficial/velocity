f15de0187433b866159b5add07195358e56e9e24
monikagore <mobnikgore5=6@m=gmail.com>
2022-10-21 18:43:40 +0530

    gallery

--- gallery.php
+++ gallery.php
@@ -1,3 +1,4 @@
+<?php include 'include\config.php';?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <!-- Mirrored from metropolitanhost.com/themes/templatemoster/html/masala/?i=150835&pr_code=z2x5P5eD1537DEbaJXgDTNkIIC33HC by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 05 Aug 2022 05:41:23 GMT -->
@@ -16,11 +17,20 @@
     <link rel="stylesheet" href="assets/fonts/flaticon/flaticon.css" />
     <link rel="stylesheet" href="assets/fonts/flaticon-2/flaticon.css" />
     <link rel="stylesheet" href="assets/fonts/font-awesome/css/all.min.css" />
-
+    <link href="glightbox/css/glightbox.min.css" rel="stylesheet">
     <!-- Organicz Style sheet -->
     <link rel="stylesheet" href="assets/css/style.css" />
     <!-- Favicon -->
     <link rel="icon" type="image/png" sizes="32x32" href="favicon.ico" />
+    <style>
+      	.gslide-description, .description-bottom{
+		display: none !important;
+	}
+  .img-fluid {
+    max-width: 100%;
+    height: 100%;
+}
+    </style>
   </head>
 
   <body>
@@ -54,33 +64,47 @@
   </div>
   <!-- Subheader End -->
 
-    <!-- Products Start -->
-    <section class="section section-padding pt-0">
-      <div class="container">
-        <div class="section-title-wrap section-header">
-          <h2 class="title">Spicy World</h2>
-          <p class="subtitle">We take care of your health...</p>
-        </div>
 
-        <div class="row">
-          <div class="col-lg-3 col-md-6">
-            <div class="ct-product">
-              <div class="ct-product-thumbnail">
-                <a href="product-details.html"
-                  ><img src="assets/img/products/2.png" alt="product"
-                /></a>
-              </div>
-              <div class="ct-product-body">
-                <h5 class="product-title text-center">
-                  <a href="product-details.html">Agro</a>
-                </h5>
-              </div>
+  <section class="py-3 mb-3">
+        <div class="container">
+            <div class="tp-title-line-2"></div>
+          
+
+               
+
+
+
+                        <div class="row portfolio-container" data-aos="fade-up">
+
+
+						<?php     
+                             $sql=mysqli_query($conn,"select * from gallery");
+
+                             while($arr=mysqli_fetch_array($sql)){
+                             ?>
+
+                            <div class="col-lg-4 col-md-6 portfolio-item filter-card py-3">
+                                
+
+                                <a href="admin/dist/img/images/<?php echo $arr['file'];?>" data-gallery="portfolioGallery"
+                                    class="portfolio-lightbox preview-link text-center" title="Trusts & Hospitals">
+                                    <img src="admin/dist/img/images/<?php echo $arr['file'];?>" class="img-fluid" alt="">
+                                </a>
+                            </div>
+							<?php }  ?>
+                        </div>
+
+               
+
+
+
+
+
             </div>
-          </div>
-        </div>
-      </div>
+
     </section>
-    <!-- Products End -->
+
+
 
 
 
@@ -100,9 +124,69 @@
     <script src="assets/js/plugins/jquery.countdown.min.js"></script>
     <script src="assets/js/plugins/isotope.pkgd.min.js"></script>
     <script src="assets/js/plugins/slick.min.js"></script>
+    <script src="glightbox/js/glightbox.min.js"></script>
+
 
     <!-- Organicz Scripts -->
     <script src="assets/js/main.js"></script>
+
+    
+<script>
+	
+  /**
+   * Porfolio isotope and filter
+   */
+  window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item'
+      });
+
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on('click', '#portfolio-flters li', function(e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        portfolioIsotope.on('arrangeComplete', function() {
+          AOS.refresh()
+        });
+      }, true);
+    }
+
+  });
+
+  /**
+   * Initiate portfolio lightbox 
+   */
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+  });
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+</script>
   </body>
 
   <!-- Mirrored from metropolitanhost.com/themes/templatemoster/html/masala/?i=150835&pr_code=z2x5P5eD1537DEbaJXgDTNkIIC33HC by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 05 Aug 2022 05:50:23 GMT -->
